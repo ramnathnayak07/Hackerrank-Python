4f687cb918b73995bbf12d267dc4843e7eaf2950
Ramnath Nayak <ramnathnayak22@gmail.com>
2024-09-16 14:57:13 +0530

    added new files

---
+++ Setdiscardremoveamppop.py
@@ -0,0 +1,15 @@
+number_of_elements = int(input())
+elements = set(map(int, input().split()))
+number_of_commands = int(input())
+for _ in range(number_of_commands):
+    cmd = list(input().split())
+    if len(cmd) == 1:
+        elements.pop()
+    else:
+        value = int(cmd[1])
+        operation = cmd[0]
+        if operation == "discard":
+            elements.discard(value)
+        else:
+            elements.remove(value)
+print(sum(elements))
