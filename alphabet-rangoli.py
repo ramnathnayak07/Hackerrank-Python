8e6800e199f78f7b360450544fde296bf6d13298
Ramnath Nayak <ramnathnayak22@gmail.com>
2024-09-27 20:16:17 +0530

    Update alphabet-rangoli.py

--- alphabet-rangoli.py
+++ alphabet-rangoli.py
@@ -6,7 +6,6 @@ def print_rangoli(size):
         s = "-".join(a[i:size])
         r.append((s[::-1]+s[1:]).center(4*size-3, "-"))
     print('\n'.join(r[:0:-1]+r))
-
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
