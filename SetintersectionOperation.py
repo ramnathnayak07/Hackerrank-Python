8e1a69e3b96f1ffba29980a35da482fc2fcebcb4
Ramnath Nayak <ramnathnayak22@gmail.com>
2024-09-28 19:11:47 +0530

    Update SetintersectionOperation.py

--- SetintersectionOperation.py
+++ SetintersectionOperation.py
@@ -3,5 +3,3 @@ english_subscribers = set(map(int, input().split()))
 number_of_french_subscribers = input()
 french_subscribers = set(map(int, input().split()))
 print(len(english_subscribers.intersection(french_subscribers)))
-# Alternative solution
-# print(len(english_subscribers & french_subscribers))
